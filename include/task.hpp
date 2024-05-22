6dd158717afe56e47b527d201a013f0184eee08a
Bowen Ding <bowendean_cow@foxmail.com>
2024-05-22 12:10:31 +0000

    task<T> ver.1

--- include/task.hpp
+++ include/task.hpp
@@ -5,11 +5,101 @@
 #include <utility>
 #include <string>
 #include <fmt/core.h>
+#include <concepts>
+#include <type_traits>
 
 namespace co_api {
+
 template <class T>
 struct task {
-  struct promise_type {};
+  // task<T>作为协程返回值需要实现promise_type
+  struct promise_type {
+    // 懒启动
+    constexpr std::suspend_always initial_suspend() noexcept { return {}; }
+
+    // co_return时要恢复在awaiting的协程
+    constexpr auto final_suspend() noexcept {
+      struct final_awaiter {
+        constexpr bool await_ready() noexcept { return false; }
+
+        std::coroutine_handle<> await_suspend(
+            std::coroutine_handle<promise_type> h) noexcept {
+          // 存在awaitee，该task是被co_await的，完成后resume被suspend的coroutine
+          if (h.promise().continuation) {
+            return h.promise().continuation;
+          } else {  // 该task是被直接启动的，没有被co_await，完成后退出
+            return std::noop_coroutine();
+          }
+        }
+        void await_resume() noexcept {}
+      };
+      return final_awaiter{};
+    }
+
+    void unhandled_exception() {
+      // todo: exception
+    };
+
+    // 构造task以返回
+    task get_return_object() {
+      return task{std::coroutine_handle<promise_type>::from_promise(*this)};
+    };
+
+    void return_value(T&& value) {
+      // todo: fill the result
+      result = std::forward<T>(value);
+    }
+
+    // 记录本协程结束后将要唤醒的协程
+    std::coroutine_handle<> continuation;
+    T result;
+  };
+
+  // 为了能在另一个task<T>里被co_await需要为其实现awaiter
+  struct awaiter_type {
+    friend task;
+    // 当co_await inner_task()时首先确认是否需要等待
+    constexpr bool await_ready() noexcept { return false; }
+
+    // 该task被co_await时，记录原coroutine，启动该task
+    auto await_suspend(std::coroutine_handle<> awaitee) noexcept {
+      coro.promise().continuation = awaitee;
+      return coro;
+    }
+
+    T await_resume() {
+      // todo: return the result
+      return std::forward<T>(coro.promise().result);
+    }
+
+   private:
+    explicit awaiter_type(std::coroutine_handle<promise_type> h) noexcept
+        : coro(h) {}
+
+    std::coroutine_handle<promise_type> coro;  // 被co_await的task
+  };
+
+  awaiter_type operator co_await() && { return awaiter_type{self}; }
+
+  // move construction
+  task(task&& other) : self(std::exchange(other.self, {})){};
+
+  ~task() {
+    if (self) {
+      self.destroy();
+    }
+  }
+
+  // void emit() {
+  //   if (self) {
+  //     self.resume();
+  //   }
+  // }
+
+ private:
+  explicit task(std::coroutine_handle<promise_type> h) : self(h) {}
+  std::coroutine_handle<promise_type> self;
+  // T result;
 };
 
 template <class = void>
@@ -30,8 +120,8 @@ struct task<void> {
         std::coroutine_handle<> await_suspend(
             std::coroutine_handle<promise_type> h) noexcept {
           // 存在awaitee，该task是被co_await的，完成后resume被suspend的coroutine
-          if (h.promise().awaitee) {
-            return h.promise().awaitee;
+          if (h.promise().continuation) {
+            return h.promise().continuation;
           } else {  // 该task是被直接启动的，没有被co_await，完成后退出
             return std::noop_coroutine();
           }
@@ -53,7 +143,7 @@ struct task<void> {
     void return_void() noexcept {};
 
     // 记录本协程结束后将要唤醒的协程
-    std::coroutine_handle<> awaitee;
+    std::coroutine_handle<> continuation;
   };
 
   // 为了能在另一个task<T>里被co_await需要为其实现awaiter
@@ -64,7 +154,7 @@ struct task<void> {
 
     // 该task被co_await时，记录原coroutine，启动该task
     auto await_suspend(std::coroutine_handle<> awaitee) noexcept {
-      coro.promise().awaitee = awaitee;
+      coro.promise().continuation = awaitee;
       return coro;
     }
 
